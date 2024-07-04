35d8ea29005adc8c77785d88d10d43f01a35e39f
Lucas Gorrie <n11277211@qut.edu.au>
2024-07-05 09:44:43 +1000

    Added PGP verification skeleton

---
+++ API/Controllers/PGPVerify.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using DidiSoft.Pgp;
+
+namespace API.Controllers
+{
+    class PGPVerify
+    {
+        public static string CommunicationSign()
+        {
+            // message to be signed
+            string plainString = "Hello World";
+
+            // create an instance of the library
+            PGPLib pgp = new PGPLib();
+
+            // sign
+            string signedString =
+            pgp.SignString(plainString,
+                       new FileInfo(@".private_key.asc"),
+                       "private key password");
+
+            Console.WriteLine(signedString);
+            return signedString;
+        }
+        public static bool CommunicationVerify()
+        {
+            // create an instance of the library
+            PGPLib pgp = new PGPLib();
+
+            // check the signature and extract the data 
+            SignatureCheckResult signatureCheck =
+                pgp.VerifyFile(@"C:\Test\INPUT.pgp", // Input
+                               @"C:\Test\public_key.asc", // Public key
+                               @"C:\Test\OUTPUT.txt"); // Output
+
+            if (signatureCheck == SignatureCheckResult.SignatureVerified)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
