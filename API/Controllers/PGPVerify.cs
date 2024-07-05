0cd23a50572af3b54c4ac5eb20ef476871d49b7a
Lucas Gorrie <n11277211@qut.edu.au>
2024-07-05 10:19:59 +1000

    Updated PGPVerify class to accept hardcoded keys

--- API/Controllers/PGPVerify.cs
+++ API/Controllers/PGPVerify.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.IO;
-using DidiSoft.Pgp;
+﻿using DidiSoft.Pgp;
 
 namespace API.Controllers
 {
     class PGPVerify
     {
-        public static string CommunicationSign()
-        {
+        public static string CommunicationSign(){
             // message to be signed
             string plainString = "Hello World";
 
@@ -17,29 +14,25 @@ namespace API.Controllers
             // sign
             string signedString =
             pgp.SignString(plainString,
-                       new FileInfo(@".private_key.asc"),
+                       new FileInfo(@".\Keys\private.asc"), // Private key
                        "private key password");
 
             Console.WriteLine(signedString);
             return signedString;
         }
-        public static bool CommunicationVerify()
-        {
+        public static bool CommunicationVerify(){
             // create an instance of the library
             PGPLib pgp = new PGPLib();
 
             // check the signature and extract the data 
             SignatureCheckResult signatureCheck =
-                pgp.VerifyFile(@"C:\Test\INPUT.pgp", // Input
-                               @"C:\Test\public_key.asc", // Public key
-                               @"C:\Test\OUTPUT.txt"); // Output
+                pgp.VerifyFile(@"", // Input <-- ADD
+                               @".\Keys\public.asc");// Public key
 
-            if (signatureCheck == SignatureCheckResult.SignatureVerified)
-            {
+            if (signatureCheck == SignatureCheckResult.SignatureVerified){
                 return true;
             }
-            else
-            {
+            else{
                 return false;
             }
         }
